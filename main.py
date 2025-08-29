da9500b83a18d408261cd5e28890f83d21a7ed1c
marquesafonso <afonso.cunha.marques@gmail.com>
2025-08-29 17:47:49 +0200

    add stratified train test split + id2label mapping (wip)

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from src.models import TicketTriageModel
 
 def main():
     dataset, queue_labels = load_dataset()
-    dataset = dataset.take(300)
+    test_dataset, train_dataset = dataset["test"], dataset["train"]
 
     model_name = "microsoft/deberta-xlarge"
     model = TicketTriageModel(labels=queue_labels)
 
-    dataset = model.process_dataset(dataset, batch_size=256)
+    test_dataset = model.process_dataset(test_dataset, batch_size=256)
 
-    dataset.to_parquet(f"dataset_preds_{model_name.replace('/', '_')}.parquet")
+    test_dataset.to_parquet(f"dataset_preds_{model_name.replace('/', '_')}_test.parquet")
 
-    accuracy = dataset.filter(lambda x: x["queue"] == x["pred_queue"]).num_rows * 100 / dataset.num_rows
+    accuracy = test_dataset.filter(lambda x: x["queue"] == x["pred_queue"]).num_rows * 100 / dataset.num_rows
     print(accuracy)
 
 
--- src/dataset.py
+++ src/dataset.py
@@ -17,7 +17,6 @@ def load_dataset():
     queue_labels = df.unique("queue")
     # df = df.to_iterable_dataset()
 
-    df.to_iterable_dataset()
     df_en = df.filter(lambda x: x["language"] == "en")
     df_en = df_en.select_columns(["subject", "body", "queue"])
     df_en = df_en.map(lambda x: {
@@ -28,4 +27,11 @@ def load_dataset():
     df_en = df_en.map(lambda x: {
         "ticket": x.get("subject") + "\n" + x.get("body")
         })
+    df_en = df_en.class_encode_column("queue")
+    
+    # TODO: verify correctness of id2label mapping
+    id2label = {i: label for i, label in enumerate(queue_labels.names)}
+    label2id = {label: i for i, label in enumerate(queue_labels.names)}
+
+    df_en = df_en.train_test_split(test_size=0.25, train_size=0.75, stratify_by_column="queue", seed=42)
     return df_en, queue_labels
