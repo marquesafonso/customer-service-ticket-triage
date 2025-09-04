3c72dfd15e02cc64d9dc5efb78b5f50a669ed6e0
marquesafonso <afonso.cunha.marques@gmail.com>
2025-09-04 20:03:02 +0200

    add model finetuning; split Base and TicketTriage models; add trackio for logging

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
+import os
 from src.dataset import load_dataset
-from src.models import TicketTriageModel
+from src.models import TicketTriageModel, BaseModel
+from huggingface_hub import login
+from dotenv import load_dotenv
+
+## TODO: improve result evaluation with compute_metrics
 
 def main():
-    dataset, queue_labels, id2labels, labels2id = load_dataset()
+    # log in to HF
+    load_dotenv()
+    HF_TOKEN = os.getenv("HF_TOKEN")
+    login(token=HF_TOKEN)
+
+    ## Prepare datasets
+    dataset, queue_labels, id2labels, labels2ids = load_dataset()
     test_dataset, train_dataset = dataset["test"], dataset["train"]
     
-    model_name = "microsoft/deberta-xlarge"
-    model = TicketTriageModel(labels=queue_labels)
+    ## Prepare base model
+    basemodel_name = "microsoft/deberta-v3-base"
+    basemodel = BaseModel(model_name=basemodel_name, labels=queue_labels)
 
-    test_dataset = model.process_dataset(test_dataset, batch_size=256)
+    # Tokenize train_dataset for training
+    encoded_train_dataset = train_dataset.map(basemodel.preprocess_data, batched=True, remove_columns=train_dataset.column_names)
 
-    test_dataset.to_parquet(f"dataset_preds_{model_name.replace('/', '_')}_test.parquet")
+    ## finetune the model
+    basemodel.finetune_model(encoded_train_dataset, id2labels, labels2ids)
 
-    accuracy = test_dataset.filter(lambda x: x["queue"] == x["pred_queue"]).num_rows * 100 / dataset.num_rows
-    print(accuracy)
+    ## Evaluate
+    finetuned_model_name = f"marquesafonso/ticket_triage_{basemodel_name.replace('/','_')}_finetuned"
+    finetuned_model = TicketTriageModel(model_name=finetuned_model_name, labels=queue_labels)
+
+    finetuned_dataset = finetuned_model.get_predictions_from_dataset(test_dataset, batch_size=256)
+    finetuned_dataset.to_parquet(f"finetuned_preds.parquet")
+
+    finetuned_accuracy = finetuned_dataset.filter(lambda x: x["queue"] == x["pred_queue"]).num_rows * 100 / finetuned_dataset.num_rows
+    print(finetuned_accuracy)
 
 
 if __name__ == "__main__":
--- src/models.py
+++ src/models.py
@@ -1,19 +1,22 @@
 import warnings
 import datasets as ds
-from transformers import pipeline
+from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer, EvalPrediction
 import torch
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
+import numpy as np
+import trackio
 
 class TicketTriageModel:
     def __init__(self,
                 labels: list,
-                model_name = "MoritzLaurer/DeBERTa-v3-large-mnli-fever-anli-ling-wanli"):
+                model_name = "microsoft/deberta-v3-base"):
         warnings.filterwarnings("ignore")
         self.model_name = model_name
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.classifier = pipeline("zero-shot-classification", model=self.model_name, device=self.device)
         self.labels = labels
 
-    def process_dataset(self, dataset: ds.Dataset, batch_size: int = 32) -> ds.Dataset:
+    def get_predictions_from_dataset(self, dataset: ds.Dataset, batch_size: int = 32) -> ds.Dataset:
         """
         Run batch inference on a Hugging Face Dataset and add predictions as a column.
         """
@@ -27,4 +30,98 @@ class TicketTriageModel:
                 return {"pred_queue": [out["labels"][0] for out in outputs]}
 
         return dataset.map(predict, batched=True, batch_size=batch_size)
+
+class BaseModel:
+    def __init__(self,
+                labels: list,
+                model_name = "microsoft/deberta-v3-base"):
+        warnings.filterwarnings("ignore")
+        self.model_name = model_name
+        self.labels = labels    
+    # source: https://jesusleal.io/2021/04/21/Longformer-multilabel-classification/
+    def multi_label_metrics(self, predictions, labels, threshold=0.5):
+        # first, apply sigmoid on predictions which are of shape (batch_size, num_labels)
+        sigmoid = torch.nn.Sigmoid()
+        probs = sigmoid(torch.Tensor(predictions))
+        # next, use threshold to turn them into integer predictions
+        y_pred = np.zeros(probs.shape)
+        y_pred[np.where(probs >= threshold)] = 1
+        # finally, compute metrics
+        y_true = labels
+        f1_micro_average = f1_score(y_true=y_true, y_pred=y_pred, average='micro')
+        roc_auc = roc_auc_score(y_true, y_pred, average = 'micro')
+        accuracy = accuracy_score(y_true, y_pred)
+        # return as dictionary
+        self.metrics = {
+            'f1': f1_micro_average,
+            'roc_auc': roc_auc,
+            'accuracy': accuracy
+            }
+        return self.metrics
+
+    def compute_metrics(self, p: EvalPrediction):
+        preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
+        self.result = self.multi_label_metrics(predictions=preds, labels=p.label_ids)
+        return self.result
+    
+    def preprocess_data(self, dataset):
+        # take a batch of texts
+        text = list(dataset["ticket"])
+        # encode them
+        encoding = self.tokenizer(text, padding="max_length", truncation=True, max_length=128)
+        # add labels
+        labels_batch = list(dataset["queue_encoded"])
+        # create numpy array of shape (batch_size, num_labels)
+        labels_matrix = np.zeros((len(text), len(self.labels)))
+        # fill numpy array
+        for idx, _ in enumerate(self.labels):
+            labels_matrix[:, idx] = labels_batch[idx]
+
+        encoding["labels"] = labels_matrix.tolist()
+
+        return encoding
+            
+    def finetune_model(self,
+                       train_dataset,
+                       id2label,
+                       label2id,
+                       batch_size = 8,
+                       metric_name = "f1"):
+        # load model and tokenizer
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name, 
+                                                           problem_type="multi_label_classification", 
+                                                           num_labels=len(self.labels),
+                                                           id2label=id2label,
+                                                           label2id=label2id)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+
+        # tokenize and prepare the training dataset for training
+        self.encoded_train_dataset = self.preprocess_data(train_dataset)
+        
+        # initialize training args
+        args = TrainingArguments(
+            f"marquesafonso/ticket_triage_{self.model_name.replace('/','_')}_finetuned",
+            save_strategy = "epoch",
+            learning_rate=2e-5,
+            per_device_train_batch_size=batch_size,
+            per_device_eval_batch_size=batch_size,
+            num_train_epochs=5,
+            weight_decay=0.01,
+            metric_for_best_model=metric_name,
+            label_names = self.labels,
+            report_to="trackio",
+            push_to_hub=True
+        )
+
+        # initialize the trainer
+        self.trainer = Trainer(
+            self.model,
+            args,
+            train_dataset=self.encoded_train_dataset,
+            tokenizer=self.tokenizer,
+            compute_metrics=self.compute_metrics
+        )
+
+        # fine tune the model
+        self.trainer.train()
 
